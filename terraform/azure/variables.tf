e9a4b28a11f13ae17027fec91332b22a6e8e2d9d
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2023-03-15 14:35:50 +0100

    fix tf vars and gcp available machines

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -8,6 +8,12 @@ variable "customer" {}
 variable "azure_cred" {
   description = "The azure credential used to deploy the infrastructure. It contains subscription_id, tenant_id, client_id, and client_secret"
 }
+variable "azure_location" {
+    default = "West Europe"
+}
+variable "rg_name" {
+    default = ""
+}
 
 
 #
